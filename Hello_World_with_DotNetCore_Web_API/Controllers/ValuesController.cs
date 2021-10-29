a3b1799c088315a0181ccecfc6e9b2459650c0f6
201118026 <74261781+201118026@users.noreply.github.com>
2021-10-29 17:45:32 +0300

    Add project files.

---
+++ Hello_World_with_DotNetCore_Web_API/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using Hello_World_with_DotNetCore_Web_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hello_World_with_DotNetCore_Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public List<AppMessage> Get()
+        {
+            var messages = new List<AppMessage>()
+            {
+                new AppMessage { Message = "hello world"},
+                new AppMessage {Message = "hello Samu"},
+                new AppMessage {Message = "hello MVC"},
+                new AppMessage {Message = "Hello Razor Pages"},
+                new AppMessage {Message = "hello WEB API"}
+            };
+            return messages;
+        }
+    }
+}
---
+++ Hello_World_with_DotNetCore_Web_API/Models/AppMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hello_World_with_DotNetCore_Web_API.Models
+{
+    public class AppMessage
+    {
+        public string Message { get; set; }
+    }
+}
